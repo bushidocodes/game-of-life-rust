14eaee75b0e489497b1159e198dda9b0b1dfabfd
Kait <kaitmore@gmail.com>
2019-08-30 16:25:07 -0400

    created functions for building and drawing game board
    Co-authored-by: Sean McBride <spmcbride1201@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,50 @@
-extern crate termi_graphics;
-use termi_graphics::pixel_art::{PixelColors, Screen};
-
 // Any live cell with fewer than two live neighbors dies, as if caused by under population.
 // Any live cell with two or three live neighbors lives on to the next generation.
 // Any live cell with more than three live neighbors dies, as if by overpopulation.
 // Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+use rand::Rng;
 
 fn main() {
-    let mut shape1 = Vec::new();
+    const dimension : i8 = 16;
 
-    //smily face
-    shape1.push(vec![2, 2, 0, 2, 2]);
-    shape1.push(vec![2, 2, 0, 2, 2]);
-    shape1.push(vec![0, 0, 0, 0, 0]);
-    shape1.push(vec![0, 0, 1, 0, 0]);
-    shape1.push(vec![8, 0, 0, 0, 8]);
-    shape1.push(vec![8, 8, 8, 8, 8]);
-    let mut screen1 = Screen::new(8, 8, PixelColors::Black).unwrap();
-    screen1.attach_pixels(&shape1, 1, 1).unwrap();
-    //simply print it once:
-    screen1.print_screen();
-    // println!("Hello, world!");
+    #[derive(Debug)]
+    enum Cell {
+        Alive,
+        Dead,
+    }
+
+    let mut gameboard: Vec<Vec<Cell>> = Vec::new();
+    
+    fn buildGameBoard(mut gameboard: Vec<Vec<Cell>>) -> Vec<Vec<Cell>> {
+        let mut rng = rand::thread_rng();
+        for row in 0..dimension {
+            let mut row: Vec<Cell> = Vec::new();
+            for cell in 0..dimension {
+                let isAlive: i8 = rng.gen_range(0, 2);
+                if isAlive == 1 {
+                    row.push(Cell::Alive);
+                } else {
+                    row.push(Cell::Dead);
+                }
+            }
+            gameboard.push(row);
+        }
+        gameboard
+    }
+
+    fn drawGameBoard(gameboard: Vec<Vec<Cell>>) {
+        for row in gameboard {
+            for cell in row {
+                match cell {
+                    Cell::Alive => print!(" * "),
+                    Cell::Dead => print!("   "),
+                }
+            }
+            println!();
+        }
+
+    }
+    gameboard = buildGameBoard(gameboard);
+    println!("{:?}", gameboard);
+    drawGameBoard(gameboard);
 }
